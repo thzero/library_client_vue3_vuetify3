f84cf4ca22ac5186b5e80a1e8a285a38f7ed1c01
thzero <thzero@users.noreply.github.com>
2023-06-07 08:16:06 -0500

    refactor to better display on small sceens

--- components/VOpenSource.vue
+++ components/VOpenSource.vue
@@ -3,137 +3,145 @@
 		<v-card-text
 			class="pt-2"
 		>
-			<v-row
-			>
+			<v-row dense>
 				<v-col
-					cols="12"
-					lg="6"
-					mb-1
-					:pr-2="$vuetify.display.lgAndUp"
+					cols="0"
+					md="2"
 				>
-					<v-table
-						fixed-header
-						style="width: 100%;"
-					>
-						<thead>
-							<tr>
-								<th class="text-center" colspan="2">
-									{{ $t('openSource.client') }}
-								</th>
-							</tr>
-							<tr>
-								<th class="text-left">
-									{{ $t('openSource.resource') }}
-								</th>
-								<th class="text-left">
-									{{ $t('openSource.license') }}
-								</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr
-								v-for="(item2, jndex) in dependenciesClient"
-								:key="key(jndex, 'b')"
-							>
-								<td class="text-left">
-									<a
-										v-if="item2.url"
-										class="opensource-padding"
-										:href="item2.url"
-									>
-										{{ item2.name }}
-									</a>
-									<span
-										v-if="!item2.url"
-										class="opensource-padding"
-									>
-										{{ item2.name }}
-									</span>
-								</td>
-								<td class="text-left">
-									<a
-										v-if="item2.licenseUrl"
-										class="opensource-padding"
-										:href="item2.licenseUrl"
-									>
-										{{ item2.licenseName }}
-									</a>
-									<span
-										v-if="!item2.licenseUrl"
-										class="opensource-padding"
-									>
-										{{ item2.licenseName }}
-									</span>
-								</td>
-							</tr>
-						</tbody>
-					</v-table>
 				</v-col>
 				<v-col
 					cols="12"
-					lg="6"
-					mb-1
-					:pl-2="$vuetify.display.lgAndUp"
+					md="8"
+					class="text-center"
 				>
-					<v-table
-						fixed-header
-						style="width: 100%;"
+					<v-row dense>
+						<v-col
+							cols="12"
+							class="text-center"
+						>
+							{{ $t('openSource.client') }}
+						</v-col>
+					</v-row>
+					<v-row dense>
+						<v-col
+							cols="9"
+							class="text-left"
+						>
+							{{ $t('openSource.resource') }}
+						</v-col>
+						<v-col
+							cols="3"
+							class="text-left"
+						>
+							{{ $t('openSource.license') }}
+						</v-col>
+					</v-row>
+					<v-row
+						v-for="(item2, jndex) in dependenciesClient"
+						:key="key(jndex, 'b')"
+						dense
 					>
-						<template #default>
-							<thead>
-								<tr>
-									<th class="text-center" colspan="2">
-										{{ $t('openSource.server') }}
-									</th>
-								</tr>
-								<tr>
-									<th class="text-left">
-										{{ $t('openSource.resource') }}
-									</th>
-									<th class="text-left">
-										{{ $t('openSource.license') }}
-									</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr
-									v-for="(item2, jndex) in dependenciesServer"
-									:key="key(jndex, 'b')"
-								>
-									<td class="text-left">
-										<a
-											v-if="item2.url"
-											class="opensource-padding"
-											:href="item2.url"
-										>
-											{{ item2.name }}
-										</a>
-										<span
-											v-if="!item2.url"
-											class="opensource-padding"
-										>
-											{{ item2.name }}
-										</span>
-									</td>
-									<td class="text-left">
-										<a
-											v-if="item2.licenseUrl"
-											class="opensource-padding"
-											:href="item2.licenseUrl"
-										>
-											{{ item2.licenseName }}
-										</a>
-										<span
-											v-if="!item2.licenseUrl"
-											class="opensource-padding"
-										>
-											{{ item2.licenseName }}
-										</span>
-									</td>
-								</tr>
-							</tbody>
-						</template>
-					</v-table>
+						<v-col
+							cols="9"
+							class="text-left"
+						>
+							<a
+								v-if="item2.url"
+								class="opensource-padding"
+								:href="item2.url"
+							>
+								{{ item2.name }}
+							</a>
+							<span
+								v-if="!item2.url"
+								class="opensource-padding"
+							>
+								{{ item2.name }}
+							</span>
+						</v-col>
+						<v-col
+							cols="3"
+							class="text-left"
+						>
+							<a
+								v-if="item2.licenseUrl"
+								class="opensource-padding"
+								:href="item2.licenseUrl"
+							>
+								{{ item2.licenseName }}
+							</a>
+							<span
+								v-if="!item2.licenseUrl"
+								class="opensource-padding"
+							>
+								{{ item2.licenseName }}
+							</span>
+						</v-col>
+					</v-row>
+					<v-row dense>
+						<v-col
+							cols="12"
+							class="text-center"
+						>
+							{{ $t('openSource.server') }}
+						</v-col>
+					</v-row>
+					<v-row dense>
+						<v-col
+							cols="9"
+							class="text-left"
+						>
+							{{ $t('openSource.resource') }}
+						</v-col>
+						<v-col
+							cols="3"
+							class="text-left"
+						>
+							{{ $t('openSource.license') }}
+						</v-col>
+					</v-row>
+					<v-row
+						v-for="(item2, jndex) in dependenciesServer"
+						:key="key(jndex, 'b')"
+						dense
+					>
+						<v-col
+							cols="9"
+							class="text-left"
+						>
+							<a
+								v-if="item2.url"
+								class="opensource-padding"
+								:href="item2.url"
+							>
+								{{ item2.name }}
+							</a>
+							<span
+								v-if="!item2.url"
+								class="opensource-padding"
+							>
+								{{ item2.name }}
+							</span>
+						</v-col>
+						<v-col
+							cols="3"
+							class="text-left"
+						>
+							<a
+								v-if="item2.licenseUrl"
+								class="opensource-padding"
+								:href="item2.licenseUrl"
+							>
+								{{ item2.licenseName }}
+							</a>
+							<span
+								v-if="!item2.licenseUrl"
+								class="opensource-padding"
+							>
+								{{ item2.licenseName }}
+							</span>
+						</v-col>
+					</v-row>
 				</v-col>
 			</v-row>
 		</v-card-text>
